1396e5b1d550892ad39cf067ff4dc37a14df4f75
Beatriz-Furtado <furtadoannabeatriz@gmail.com>
2026-07-03 10:33:21 -0300

    fix: ajustando informações

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -681,7 +681,7 @@ function AppContent() {
                           
                           {isPaperRegistration && (
                             <span className="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-amber-100 text-amber-800 border border-amber-200">
-                              Nova Data
+                              New Date
                             </span>
                           )}
                         </div>
